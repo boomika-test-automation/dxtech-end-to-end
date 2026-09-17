53561983d56ccb5564b7d371cfd772b7daa84566
Sathya Thangam <sathya.thangam@savethechildren.org>
2026-09-17 15:40:10 +0530

    Removed the unwanted files

--- pages/DxTechPage.js
+++ pages/DxTechPage.js
@@ -8,25 +8,6 @@ export class DxTechPage {
     this.email = page.getByRole('textbox', { name: 'Email address' });
     this.password = page.getByRole('textbox', { name: 'Password' });
     this.continueBtn = page.getByRole('button', { name: 'Continue' });
-
-    // Common
-    this.emptyButton = page.getByRole('button').filter({ hasText: /^$/ });
-
-    // Navigation
-    this.publishersLink = page.getByRole('link', { name: 'Publishers' });
-    this.placementsLink = page.getByRole('link', { name: 'Placements' });
-    this.appsLink = page.getByRole('link', { name: 'Apps' });
-    this.listsLink = page.getByRole('link', { name: 'Lists' });
-
-    // Publishers
-    this.search = page.getByRole('textbox', { name: 'Search' });
-    this.dashaLink = page.getByRole('link', { name: 'Dasha Test' });
-    this.appsTab = page.getByRole('tab', { name: 'Apps', exact: true });
-    this.domainsTab = page.getByRole('tab', { name: 'Domains' });
-    this.placementsTab = page.getByRole('tab', { name: 'Placements' });
-    this.manageDomains = page.getByRole('button', { name: 'Manage domains' });
-    this.cancelBtn = page.getByRole('button', { name: 'Cancel' });
-    this.editMenu = page.getByRole('menuitem', { name: 'Edit' });
   }
 
   async login() {
