641775fc2cb9da20e9e831668fb0fb71e79420fb
Sathya Thangam <sathya.thangam@savethechildren.org>
2026-09-17 16:47:03 +0530

    Added the new test flow

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -9,3 +9,11 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
